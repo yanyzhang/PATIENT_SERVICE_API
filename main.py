2a410021e8b15def59ecdb4a122fc7a0ad61df57
Yan Zhang <yanyzhang1@gmail.com>
2024-03-06 11:35:40 -0500

    updated main.py for FastAPI

---
+++ main.py
@@ -0,0 +1,27 @@
+# app.py to print a message 
+'''
+Author: Yan
+Purpose:
+Date:
+
+
+'''
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/users")
+async def get_users():
+    return [
+        {
+            "firstName": "Yan",
+            "lastName": "Zhang",
+            "email": "yanyzhang1@gmail.com"
+        }
+
+    ]
